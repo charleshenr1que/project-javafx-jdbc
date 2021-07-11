47f98d96e76c2e5dccb1e39121369213340034bb
Charles Henrique <charlessssoares00@gmail.com>
2021-07-11 20:24:21 -0300

    Delete from Department methods

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -2,11 +2,17 @@ package gui;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.Optional;
 import java.util.ResourceBundle;
+
 import application.Main;
+import db.DbException;
 import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
+import gui.util.Utils;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -16,6 +22,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -37,6 +45,12 @@ public class DepartmentListController implements Initializable, DataChangeListen
 	private TableColumn<Department, Integer> tableColumId;
 	@FXML
 	private TableColumn<Department, String> tableComlumName;
+
+	@FXML
+	private TableColumn<Department, Department> tableClomunEdit;
+
+	@FXML
+	private TableColumn<Department, Department> tableClomunDelete;
 
 	@FXML
 	public void onBtNewActpio(ActionEvent event) {
@@ -80,6 +94,8 @@ public class DepartmentListController implements Initializable, DataChangeListen
 		List<Department> list = service.findAll();
 		obsList = FXCollections.observableArrayList(list);
 		tableViewDepartment.setItems(obsList);
+		initEditButtons();
+		deleteButtons();
 
 	}
 
@@ -110,6 +126,66 @@ public class DepartmentListController implements Initializable, DataChangeListen
 	@Override
 	public void onDataChanged() {
 		initializeDepartment();
+	}
+
+	@FXML
+	private void initEditButtons() {
+		tableClomunEdit.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+		tableClomunEdit.setCellFactory(param -> new TableCell<Department, Department>() {
+			private final Button button = new Button("edit");
+
+			@Override
+			protected void updateItem(Department obj, boolean empty) {
+				super.updateItem(obj, empty);
+				if (obj == null) {
+					setGraphic(null);
+					return;
+				}
+				setGraphic(button);
+				button.setOnAction(
+						event -> createDialogForm(obj, "/gui/DepartmentForm.fxml", Utils.currentStage(event)));
+			}
+		});
+
+	}
+
+	private void deleteButtons() {
+		tableClomunDelete.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+		tableClomunDelete.setCellFactory(param -> new TableCell<Department, Department>() {
+			private final Button button = new Button("delete");
+
+			@Override
+			protected void updateItem(Department obj, boolean empty) {
+				super.updateItem(obj, empty);
+				if (obj == null) {
+					setGraphic(null);
+					return;
+				}
+				setGraphic(button);
+				button.setOnAction(event -> removeEntity(obj));
+			}
+		});
+
+	}
+
+
+	private void removeEntity(Department obj) {
+	Optional <ButtonType> result = Alerts.showConfirmation("Confirmation", "do you want to remove?");
+		
+	if(result.get() == ButtonType.OK) {
+		if(service == null) {
+			throw new IllegalStateException("Service was null");
+		}try {
+			service.removeDepartment(obj);
+			initializeDepartment();
+		}
+		catch(DbException e) {
+			Alerts.showAlert("Error removing object", null, e.getMessage(), AlertType.ERROR);
+		}
+		
+		
+	}
+		
 	}
 
 }
